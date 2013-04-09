e18489689901f760121865a673eb01dc4d4c016b
Roy Storey <roy.storey@plantandfood.co.nz>
2013-04-09 20:26:19 +1200

    installed apps and lavaflow root
    Signed-off-by: Roy Storey <roy.storey@plantandfood.co.nz>

--- lavaFlow/settings.py
+++ lavaFlow/settings.py
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.abspath(os.path.join('.', 'lavaFlow', 'templates'))
+    os.path.abspath(os.path.join(os.getenv('LAVAFLOW_ROOT'), 'lavaFlow', 'templates'))
 )
 
 INSTALLED_APPS = (
@@ -130,7 +130,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'lavaFlow',
 )
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
